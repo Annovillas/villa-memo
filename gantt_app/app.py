098ffff880db958bc9fff31b8fefaf10a8b275fd
annovillas <annotakesan@gmail.com>
2025-08-22 23:42:35 +0900

    Add standalone gantt_app (Gantt-like staffing with memos)

---
+++ gantt_app/app.py
@@ -0,0 +1,6 @@
+git status                      # 看看變更
+git add gantt_app/app.py
+git commit -m "Add standalone gantt_app (Gantt-like staffing with memos)"
+git push                        # 推到 origin/main
+
+
